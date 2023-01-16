0983a5a33100d57e2e4bb987fcf402c180c7648e
Birhan <58909711+fiker1@users.noreply.github.com>
2023-01-16 14:28:45 -0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'oh wellcome to love angular project first time cration after debug error successfully!! ';
+}
